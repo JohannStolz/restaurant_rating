ad9dacacee63a29e05ee149c4c21a12c54281c05
JohannStolz <shtolz35@gmail.com>
2018-09-17 20:32:23 +0300

    refactored by test results ValidationUtil

--- src/main/java/com/graduate/restaurant_rating/util/ValidationUtil.java
+++ src/main/java/com/graduate/restaurant_rating/util/ValidationUtil.java
@@ -1,6 +1,7 @@
 package com.graduate.restaurant_rating.util;
 
 import com.graduate.restaurant_rating.domain.AbstractBaseEntity;
+import com.graduate.restaurant_rating.util.exception.NotFoundException;
 import com.graduate.restaurant_rating.util.exception.WrongIdException;
 
 import java.util.Objects;
@@ -12,10 +13,30 @@ public class ValidationUtil {
     private ValidationUtil() {
     }
 
+    public static <T> T checkNotFoundWithId(T object, int id) {
+        return checkNotFound(object, "id=" + id);
+    }
+
+
     public static void checkForMatchId(AbstractBaseEntity entity, int id) {
         Objects.requireNonNull(entity);
         if (!entity.getId().equals(id)) {
             throw new WrongIdException("id not equals");
+        }
+    }
+    public static void checkDeleteSuccess(boolean delete, int id) {
+        if (!delete) {
+            throw new NotFoundException("Not found entity with id=" + id);
+        }
+    }
+
+    public static <T> T checkNotFound(T object, String msg) {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+    public static void checkNotFound(boolean found, String msg) {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + msg);
         }
     }
 }
