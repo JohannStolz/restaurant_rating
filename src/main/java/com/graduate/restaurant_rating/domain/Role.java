5e8e1f134347e31edd386729fb13142810674d9f
JohannStolz <shtolz35@gmail.com>
2018-09-24 18:03:52 +0300

    Role implements GrantedAuthority

--- src/main/java/com/graduate/restaurant_rating/domain/Role.java
+++ src/main/java/com/graduate/restaurant_rating/domain/Role.java
@@ -1,9 +1,16 @@
 package com.graduate.restaurant_rating.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+
 /**
  * Created by Johann Stolz 14.08.2018
  */
-public enum Role {
+public enum Role implements GrantedAuthority {
     ROLE_USER,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
